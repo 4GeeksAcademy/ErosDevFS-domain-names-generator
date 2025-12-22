9721a9d57ca6dea504718facfd195fa130ebe79e
Julian Sedano <ignovigenz@hotmail.com>
2025-12-22 23:08:55 +0000

    method2

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-let pronouns = ['the', 'our'];
+let pronouns = ['the', 'our', 'he'];
 let adjs = ['great', 'big'];
 let nouns = ['jogger', 'racoon'];
 let listDomains = '';
 
-function addPronouns(pronoun){
-
-}
 
 for(let i=0; i < pronouns.length; i++){
   for(let j=0; j < adjs.length; j++){
     for(let k=0; k < nouns.length; k++)
-      document.getElementById('test') =listDomains += pronouns[i] + adjs[j] + nouns[k] + ".com" + "\n"
+      console.log(listDomains += pronouns[i] + adjs[j] + nouns[k] + ".com" + "\n")
   }
 }
 
