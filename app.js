58c3f71ca65b8ffd3dd6334190cb3e2e0de5cc32
Julian Sedano <ignovigenz@hotmail.com>
2025-12-23 16:07:11 +0000

    refactoring done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let pronouns = ['the', 'our'];
-let adjs = ['great', 'big'];
+let adjectives = ['great', 'big'];
 let nouns = ['jogger', 'racoon'];
-let listDomains = '';
+let listDomains = ''; 
 
 for(let i=0; i < pronouns.length; i++){
-  for(let j=0; j < adjs.length; j++){
+  for(let j=0; j < adjectives.length; j++){
     for(let k=0; k < nouns.length; k++)
-      listDomains += pronouns[i] + adjs[j] + nouns[k] + ".com" + "\n"
+      listDomains += pronouns[i] + adjectives[j] + nouns[k] + ".com" + "\n"
   }
 }
 
